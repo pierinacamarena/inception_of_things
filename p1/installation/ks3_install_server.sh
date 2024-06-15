381d44004c28ff52adb3ea72f8fcc321230007e3
pierinacamarena <pcamaren@student.42.fr>
2024-06-15 15:35:29 +0200

    [ADD] 2 machine configuration + ks3 install scripts

---
+++ p1/installation/ks3_install_server.sh
@@ -0,0 +1,9 @@
+      sudo apt-get update
+      sudo apt-get install -y curl
+
+      #install kubctl
+      curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+      sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+      kubectl version --client
+
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP" sh -s -
