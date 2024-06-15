53810dac71e2adc212a6aaf912cb2fa11960c53b
pierinacamarena <pcamaren@student.42.fr>
2024-06-15 23:32:05 +0200

    [ADD]: package manager for alpine + token

--- p1/installation/ks3_install_server.sh
+++ p1/installation/ks3_install_server.sh
@@ -1,9 +1,16 @@
-      sudo apt-get update
-      sudo apt-get install -y curl
+      sudo apk update                  # Update the package list
+      sudo apk add curl                # Install curl
+      sudo apk add net-tools           # Install net-tools if necessary
 
       #install kubctl
       curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
       sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
       kubectl version --client
 
-      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP" sh -s -
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP --flannel-iface=eth1"  sh -s -
+      
+      while ! [ -f /var/lib/rancher/k3s/server/node-token ]
+      do
+            sleep 1
+      done
+      cp /var/lib/rancher/k3s/server/node-token /vagrant
--- p1/installation/ks3_install_worker.sh
+++ p1/installation/ks3_install_worker.sh
@@ -1,10 +1,21 @@
-      sudo apt-get update
-      sudo apt-get install -y curl
+      sudo apk update                  # Update the package list
+      sudo apk add curl                # Install curl
+      sudo apk add net-tools           # Install net-tools if necessary
 
       #install kubctl
       curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
       sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
       kubectl version --client
 
-      #install ks3
-      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" sh -s -S
+
+      # Wait for the node token to be available
+      while ! [ -f /vagrant/node-token ]
+      do
+      sleep 1
+      done
+
+      # Read token from shared folder
+      TOKEN=$(cat /vagrant/node-token)
+
+      # Install k3s worker
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$SERVER_IP:6443 --node-ip=$WORKER_IP --flannel-iface=eth1 --token=$TOKEN" sh -s -
