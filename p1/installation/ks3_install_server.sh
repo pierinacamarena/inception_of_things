1fedb157b5e727ac5cd0ab5ec20eadec694c3b00
pierinacamarena <pcamaren@student.42.fr>
2024-06-16 18:34:31 +0200

    [FIX]: token copy to be accessed by worker

--- p1/installation/ks3_install_server.sh
+++ p1/installation/ks3_install_server.sh
@@ -1,22 +1,3 @@
-      # sudo apk update                  # Update the package list
-      # sudo apk add curl                # Install curl
-      # sudo apk add net-tools           # Install net-tools if necessary
-
-      # #install kubctl
-      # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-      # sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-      # kubectl version --client
-
-      # echo "k3s installation on node"
-      # echo "server is ${SERVER_IP}"
-      # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP --flannel-iface=eth1"  sh -s -
-      
-      # while ! [ -f /var/lib/rancher/k3s/server/node-token ]
-      # do
-      #       sleep 1
-      # done
-      # cp /var/lib/rancher/k3s/server/node-token /vagrant
-
 
 #!/bin/sh
 set -e  # Exit on error
@@ -27,29 +8,19 @@ sudo apk update
 sudo apk add curl
 sudo apk add net-tools
 
-echo "Installing kubectl..."
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
-
 echo "k3s installation on node with IP: $SERVER_IP"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP --flannel-iface=eth1" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
 
-echo "Configuring kubectl..."
-# export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-# echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.profile
 
 echo "Waiting for k3s to complete installation..."
-while ! [ -f /var/lib/rancher/k3s/server/node-token ]
+while ! sudo [ -f /var/lib/rancher/k3s/server/node-token ]
 do
     sleep 1
 done
-cp /var/lib/rancher/k3s/server/node-token /vagrant
 
-# echo "Verifying k3s installation..."
-# until kubectl get nodes; do
-#     echo "Waiting for k3s to be fully operational..."
-#     sleep 5
-# done
+echo "Copying node-token to /vagrant..."
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/
+echo "Finished copying token"
+
 
 echo "Installation and configuration completed successfully."
--- p1/installation/ks3_install_worker.sh
+++ p1/installation/ks3_install_worker.sh
@@ -1,43 +1,17 @@
-      # sudo apk update                  # Update the package list
-      # sudo apk add curl                # Install curl
-      # sudo apk add net-tools           # Install net-tools if necessary
-
-      # #install kubctl
-      # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-      # sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-      # kubectl version --client
-
-
-      # # Wait for the node token to be available
-      # while ! [ -f /vagrant/node-token ]
-      # do
-      #       sleep 1
-      # done
-
-      # # Read token from shared folder
-      # TOKEN=$(cat /vagrant/node-token)
-
-      # # Install k3s worker
-      # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$SERVER_IP:6443 --node-ip=$WORKER_IP --flannel-iface=eth1 --token=$TOKEN" sh -s -
 
       sudo apk update || { echo "Failed to update APK indices"; exit 1; }
       sudo apk add curl || { echo "Failed to install curl"; exit 1; }
       sudo apk add net-tools || { echo "Failed to install net-tools"; exit 1; }
 
-      # Install kubectl
-      curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
-      sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
-      kubectl version --client || { echo "Failed to install kubectl"; exit 1; }
-
       # Wait for the node token to be available
       while ! [ -f /vagrant/node-token ]
       do
-      sleep 1
+            echo "sleeping until getting token"
+            sleep 1
       done
 
       # Read token from shared folder
       TOKEN=$(cat /vagrant/node-token) || { echo "Failed to read token"; exit 1; }
 
       # Install k3s worker
-      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$SERVER_IP:6443 --node-ip=$WORKER_IP --flannel-iface=eth1 --token=$TOKEN" sh -s - ||
-      { echo "Failed to join k3s cluster"; exit 1; }
+      curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server=https://$SERVER_IP:6443 --node-ip=$WORKER_IP --flannel-iface=eth1 --token=$TOKEN" K3S_KUBECONFIG_MODE="644"  sh -s -
