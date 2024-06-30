ad2c2494873265e141cbe90d643acd4e98fad22d
pierinacamarena <pcamaren@student.42.fr>
2024-06-30 20:02:32 +0200

    [ADD]: Part 2 WIP

--- p2/provisions/ks3_install_configuration.sh
+++ p2/provisions/ks3_install_configuration.sh
@@ -1,16 +1,33 @@
 echo "Updating packages and installing required tools..."
-sudo yum update -y
-sudo yum install -y curl
+systemctl disable firewalld --now
+# echo "DONE DISABLING"
+
+# sudo yum update -y
+# echo "INSTALL 1"
+
+# sudo yum install -y curl
+# echo "INSTALL 2"
+
+echo "INSTALLING  NET TOOLS"
 sudo yum install -y net-tools
+echo "DONE INSTALLNG NET TOOLS"
+
 
 # Install k3s
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--cluster-init --node-ip=$SERVER_IP --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=$SERVER_IP --flannel-iface=eth1" K3S_KUBECONFIG_MODE="644" sh -s -
 
-
-mkdir 
+echo "DONE INSTALLING K3S"
+# mkdir 
 # Ensure k3s is ready and apply the Kubernetes manifest
 echo "Applying Kubernetes manifest..."
-sudo /usr/local/bin/k3s kubectl apply -f /vagrant/setup.yml
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app1/app1.deployment.yaml
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app1/app1.service.yaml
+
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app2/app2.deployment.yaml
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app2/app2.service.yaml
+
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app3/app3.deployment.yaml
+sudo /usr/local/bin/k3s kubectl apply -f /vagrant/app3/app3.service.yaml
 
 echo "Setup complete!"
